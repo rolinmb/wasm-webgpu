c0da75339ff7bcc688cc34bbe2f352b8f7e52f23
Rolin Blake <rolinmblake@gmail.com>
2024-02-05 20:41:00 -0600

    problems with array matmul

--- dist/driver.js
+++ dist/driver.js
@@ -1,25 +1,63 @@
-const matA = new Float32Array([
+var matA = new Float32Array([
   1.0, 2.0, 3.0, 4.0,
   5.0, 6.0, 7.0, 8.0,
   9.0, 10.0, 11.0, 12.0,
   13.0, 14.0, 15.0, 16.0
 ]);
-
-const matB = new Float32Array([
+var matB = new Float32Array([
   17.0, 18.0, 19.0, 20.0,
   21.0, 22.0, 23.0, 24.0,
   25.0, 26.0, 27.0, 28.0,
   29.0, 30.0, 31.0, 32.0
 ]);
-
 var ab = new Float32Array(16);
+console.log("matA:", matA);
+console.log("matB:", matB);
+console.log("ab (before):", ab);
+
+var memory = new WebAssembly.Memory({
+    initial: 512,
+    maximum: 1024
+});
+
+var exports;
 
 WebAssembly.instantiateStreaming(
-  fetch("main.wasm"), {}
+  fetch("main.wasm"),
+  {
+    js: {
+        mem: memory
+    },
+    env: {
+        emscripten_resize_heap: function(delta) {
+        memory.grow(delta);
+        }
+    }
+  }
 ).then(results => {
-  console.log("matA:", matA);
-  console.log("matB:", matB);
-  console.log("ab (before):", ab);
-  results.instance.exports.matmul(matA.buffer, matB.buffer, ab.buffer);
-  console.log("ab (after):", ab);
-});
+    exports = results.instance.exports;
+    memory = results.instance.exports.memory;
+    function encodeFloat32Arr(arr, len) {
+      var ptr = exports.wasm_malloc(len);
+      var out = new Float32Array(memory.buffer, ptr, len);  
+      for (var i = 0; i < len; i++) {
+        out[i] = arr[i];
+      }
+      return ptr;
+    }
+    function runWASM() {
+      const ptrA = encodeFloat32Arr(matA, matA.length);
+      const ptrB = encodeFloat32Arr(matB, matB.length);
+      var ptrAB = encodeFloat32Arr(ab, ab.length);
+      console.log("ptrAB (before):", ptrAB);
+      exports.matmul(ptrA, ptrB, ptrAB);
+      console.log("ab (after):", ab);
+      console.log("ptrAB (after):", ptrAB);
+      exports.wasm_free(ptrA);
+      exports.wasm_free(ptrB);
+      exports.wasm_free(ptrAB);
+    }
+    window.addEventListener("load", runWASM());
+  }
+);
+
--- src/c/main.c
+++ src/c/main.c
@@ -1,4 +1,7 @@
 #include <emscripten.h>
+#include <stdlib.h>
+
+int main() { return 0; }
 
 const int SIZE = 4;
 
@@ -6,10 +9,21 @@ EMSCRIPTEN_KEEPALIVE
 void matmul(float* A, float* B, float* ab) {
   for (int i = 0; i < SIZE; i++) {
     for (int j = 0; j < SIZE; j++) {
-      ab[i * SIZE + j] = 0.0;
+      float result = 0.0;
       for (int k = 0; k < SIZE; k++) {
-        ab[i * SIZE + j] += A[i * SIZE + k] * B[k * SIZE + j];
+        result += A[i * SIZE + k] * B[k * SIZE + j];
       }
+      ab[i * SIZE + j] = result;
     }
   }
+}
+
+EMSCRIPTEN_KEEPALIVE
+void *wasm_malloc(size_t n) {
+  return malloc(n);
+}
+
+EMSCRIPTEN_KEEPALIVE
+void wasm_free(void *ptr) {
+  return free(ptr);
 }
